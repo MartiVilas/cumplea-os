fcada821eb31a885cf93b401f37a29ccb59c445f
MartiVilas <marti.vilas14@gmail.com>
2024-03-14 17:39:21 +0100

    eliminar main.js, poner funcion  en index.html

--- main.js
+++
@@ -1,5 +0,0 @@
-function abrirVentana() {
-    // Abre una nueva ventana con las opciones especificadas
-    let opciones = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=1500,height=1200,left=500,top=300`;
-    window.open('https://www.youtube.com/watch?v=7V-qmzu6KVU&ab_channel=Pascal', '', opciones); 
-  }
