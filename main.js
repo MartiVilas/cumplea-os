d1e6f78a33f0f0778e177aa491f826305bbbd5ff
Martí Vilàs <150129703+MartiVilas@users.noreply.github.com>
2024-03-15 08:43:25 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+function abrirVentana() {
+    // Abre una nueva ventana con las opciones especificadas
+    let opciones = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=1500,height=1200,left=500,top=300`;
+    window.open('https://www.youtube.com/watch?v=7V-qmzu6KVU&ab_channel=Pascal', '', opciones); 
+}
+const boton = document.getElementById("btn");
+boton.addEventListener("click", abrirVentana());
